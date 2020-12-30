308c5d2d14a88801d6ad697c07b40148368ebfe5
lmh <495453865@qq.com>
2020-12-30 21:46:07 +0800

    validateInput 组件完成

--- article/src/App.vue
+++ article/src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <GlobalHeader :user="user"></GlobalHeader>
     <ColumnList :list="list"></ColumnList>
+    <ValidateInput :rules="rules"></ValidateInput>
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 import { defineComponent } from 'vue'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -43,16 +45,29 @@ const user: UserProps = {
   isLogin: true
 }
 
+const rules: RulesProp = [
+  {
+    type: 'required',
+    message: '请输入邮箱'
+  },
+  {
+    type: 'email',
+    message: '邮箱格式不正确'
+  }
+]
+
 export default defineComponent({
   name: 'App',
   components: {
     ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
     return {
       list: testData,
-      user
+      user,
+      rules
     }
   }
 })
---
+++ article/src/components/ValidateInput.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input
+      class="form-control"
+      :class="{'is-invalid': inputRef.error}"
+      :value="inputRef.val"
+      @input="updateValue"
+      @blur="validateInput"
+      v-bind="$attrs">
+      <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType, reactive } from 'vue'
+const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+interface RuleProp {
+  type: 'required' | 'email';
+  message: string;
+}
+
+export type RulesProp = RuleProp[]
+
+export default defineComponent({
+  name: 'ValidateInput',
+  // 拒绝组件根元素继承attribute
+  inheritAttrs: false,
+  props: {
+    rules: {
+      type: Array as PropType<RulesProp>
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props, context) {
+    const inputRef = reactive({
+      val: props.modelValue,
+      error: false,
+      message: ''
+    })
+
+    const updateValue = (e: KeyboardEvent) => {
+      const targetValue = (e.target as HTMLInputElement).value
+      inputRef.val = targetValue
+      context.emit('update:modelValue', targetValue)
+    }
+
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = inputRef.val.trim() !== ''
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            default:
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
+      }
+    }
+
+    return {
+      inputRef,
+      validateInput,
+      updateValue
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
